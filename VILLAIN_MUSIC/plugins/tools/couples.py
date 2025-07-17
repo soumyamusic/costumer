2fb105d2f80659140d7ae9a4a72a67ff18d70a76
laiba-music <kkapilgupta5084@gmail.com>
2025-07-17 19:01:02 +0530

    Update couples.py

--- VILLAIN_MUSIC/plugins/tools/couples.py
+++ VILLAIN_MUSIC/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from VILLAIN_MUSIC.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ꪜ 𝛊 ɭ ɭ ᧘ 𝛊 𝛈",
-            url=f"https://t.me/iamakki001",
+            text="⋏ 𝛅 𝛕 ⋏",
+            url=f"https://t.me/ixasta",
         ),
     ],
 ]
