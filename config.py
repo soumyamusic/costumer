766c73a803cd346ff6a027d59ecb77367140951d
villainmusic777 <villainmusicgithub@gmail.com>
2025-09-01 22:22:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 API_URL = getenv("API_URL", 'https://api.NxGBNexGenBotsdbfa2f') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots008f24') # youtube song api key
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotsdbfa2f') # youtube song api key
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/laiba-music/Laiba_music",
